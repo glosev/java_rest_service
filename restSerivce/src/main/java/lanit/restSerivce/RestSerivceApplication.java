2f161c41b6082f4442dbdbaa77477f308338dfba
glosev <130078994+glosev@users.noreply.github.com>
2023-04-08 15:12:04 +0300

    Project base
    Basic maven project with spring boot 3.0.5 and spring WEB.

---
+++ restSerivce/src/main/java/lanit/restSerivce/RestSerivceApplication.java
@@ -0,0 +1,13 @@
+package lanit.restSerivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSerivceApplication.class, args);
+	}
+
+}
